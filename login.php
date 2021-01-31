e7b5275af17f76b75b93bcbaa59d62cc53e01470
t20933gr <george.reed@student.manchester.ac.uk>
2021-01-31 21:12:46 +0000

    Implemented PHP_SELF to simplify code

--- login.php
+++ login.php
@@ -5,10 +5,28 @@
         <title></title>
     </head>
     <body>
-        <form action="doLogin.php" method="post">
+        <?php
+            if (isset($_POST['submit'])) {
+                // Get details from login form
+                $user = $_POST['user'];
+                $pass = $_POST['pass'];
+
+                // Check if user details are valid or not
+                require_once 'DBHandler.php';
+                $conn = connectDB();
+                $sql = "SELECT user, pass FROM users WHERE user='$user' AND pass='$pass'";
+                $results = doSQL($conn, $sql);
+                if ($row = $results->fetch_assoc()) {
+                    echo "pass"; // Log in
+                } else {
+                    echo "fail"; // Go back to self
+                }
+            }
+        ?>
+        <form action="<?php htmlentities($_SERVER['PHP_SELF']) ?>" method="post">
             Username: <input type="text" name="user"/><br>
             Password: <input type="password" name="pass"/><br>
-            <input type="submit" value="Log on"/>
+            <input type="submit" name="submit" value="Log on"/>
             <a href="register.php"><input type="button" value="Register"></a>
         </form>
     </body>
--- register.php
+++ register.php
@@ -5,11 +5,34 @@
         <title></title>
     </head>
     <body>
-        <form action="doRegister.php" method="post">
+        <?php
+            if (isset($_POST['submit'])) {
+                // Get details from the register form
+                $user = $_POST['user'];
+                $pass = $_POST['pass'];
+                $passCheck = $_POST['passCheck'];
+
+                // Checks if the given passwords match and adds details to database
+                // if they do
+                if ($pass == $passCheck) {
+                    require_once 'DBHandler.php';
+                    $conn = connectDB();
+                    $sql = "INSERT INTO users (user, pass)
+                            VALUES ('$user', '$pass')";
+                    $results = doSQL($conn, $sql);
+                    echo($results);
+                    // Log in
+                } else {
+                    echo "Passwords do not match";
+                    // Go back to self
+                }
+            }
+        ?>
+        <form action="<?php htmlentities($_SERVER['PHP_SELF']) ?>" method="post">
             Username: <input type="text" name="user"/><br>
             Password: <input type="password" name="pass"/><br>
             Confirm Password: <input type="password" name="passCheck"/><br>
-            <input type="submit" value="Register"/>
+            <input type="submit" name="submit" value="Register"/>
             <a href="login.php"><input type="button" value="Back"></a>
         </form>
     </body>
