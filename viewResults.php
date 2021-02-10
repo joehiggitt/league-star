166a6bba2f08a6c8b0053bbe3e3f07a82a1add02
mac <mac@MacBook-Pro.local>
2021-02-10 23:58:17 +0400

    view_results_page

---
+++ viewResults.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title> League Star - view Results</title>
+	<meta lang="en">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+	<header>
+		<h1>League Star</h1>
+	</header>
+	<dev>
+		<h2>NAME OF THE LEAGUE</h2>
+	</dev>
+	<dev>
+		<h4>Matchday (DATE)</h4>
+		table
+	</dev>
+
+	<dev>
+		<h4>Matchday (DATE)</h4>
+		table
+	</dev>
+
+</body>
+
+
+</html>
