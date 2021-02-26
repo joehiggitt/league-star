1d4f03cd79a585e9738d6ecdfc001b41a28e80c7
j22432en <eduardt.nica@student.manchester.ac.uk>
2021-02-26 11:30:10 +0000

    little change

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>LeagueStar - Create your league for free</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="LeagueStar is the perfect tool to create your own league with your friends, family or colleagues. From a new place to score your fantasy football to a fast way of recording your Among Us wins, LeagueStar can cover your needs.">
     <meta lang="en">
     <link rel="stylesheet" type="text/css" href="styles.css">
