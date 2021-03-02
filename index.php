99986862f1022786763de480de31963c249803f8
w81310jh <joseph.higgitt@student.manchester.ac.uk>
2021-03-02 13:38:42 +0000

    Created basic javascript nav writing functionality.

--- index.php
+++ index.php
@@ -21,12 +21,12 @@
 			<div class="imageText"><h1>LeagueStar</h1></div>
 		</header>
 		<nav>
-			<ul class="navNav">
+			<!-- <ul class="navNav">
 				<li><a href="index.php" id="active">Home</a></li>
 				<li><a href="about.php">About Us</a></li>
 				<li><a href="contact.php">Contact Us</a></li>
 				<li><a href="help.php">Help</a></li>
-				<?php
+				
 					// Script used if login is not required to use this page
 					if(isset($_SESSION["user"]))
 					{
@@ -38,25 +38,30 @@
 						echo '<li style="float:right"><a href="register.php">Register</a></li>';
 						echo '<li style="float:right"><a href="login.php">Sign In</a></li>';
 					}
-				?>
-			</ul>
+				
+			</ul> -->
+			<script src="writeNav.js"></script>
 		</nav>
 		<?php
 			// Script used if login is not required to use this page
 			if (isset($_SESSION["user"]))
 			{
 				echo '<aside>';
-					echo '<ul class="asideNav">';
-						echo '<li><a href="viewLeague.php">League 1</a></li>';
-						echo '<li><a href="viewTable.php">Table</a></li>';
-						echo '<li><a href="viewFixtures.php">Fixtures</a></li>';
-						echo '<li><a href="viewResults.php">Results</a></li>';
-						echo '<li><a href="createLeague.php">Create New League</a></li>';
-						echo '<li><a href="joinLeague.php">Join League</a></li>';
-					echo '</ul>';
+				echo '	<ul class="asideNav">';
+				echo '		<li><a href="viewLeague.php">League 1</a></li>';
+				echo '		<li><a href="viewTable.php">Table</a></li>';
+				echo '		<li><a href="viewFixtures.php">Fixtures</a></li>';
+				echo '		<li><a href="viewResults.php">Results</a></li>';
+				echo '		<li><a href="createLeague.php">Create New League</a></li>';
+				echo '		<li><a href="joinLeague.php">Join League</a></li>';
+				echo '	</ul>';
 				echo '</aside>';
 			}
 		?>
+		<!--<script src="writeAside.js"></script>
+		<script>
+			SCRIPT.pass(["League 1", "League 2"]);
+		</script> -->
 		<main>
 			<h2>Welcome to LeagueStar!</h2>
 			<p>LeagueStar is the perfect tool to create your own league with your friends, family or colleagues. From a new place to score your fantasy football to a fast way of recording your Among Us wins, LeagueStar can cover your needs.</p>
